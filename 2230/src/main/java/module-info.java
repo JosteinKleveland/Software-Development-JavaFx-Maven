87616e97b245bb5dc2f8b7bca5d0bb444eafaf23
William Andreas Krohn Marleau <wamarlea@stud.ntnu.no>
2022-09-20 22:09:20 +0000

    added json.simple to  module-info.java

--- 2230/src/main/java/module-info.java
+++ 2230/src/main/java/module-info.java
@@ -2,7 +2,8 @@ module CalendarApp.core {
 
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
-    opens CalendarApp.core to javafx.graphics, javafx.fxml;
+    opens CalendarApp.core to javafx.graphics, javafx.fxml, json.simple;
     
 }
