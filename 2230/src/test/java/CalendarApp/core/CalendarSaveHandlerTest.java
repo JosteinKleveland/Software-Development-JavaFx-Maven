175e220f09d6c5c6a492993f63b2ae586740b89a
Endre Mork <endremor@stud.ntnu.no>
2022-09-22 16:47:22 +0200

    Added testSetup, getFilePathTest and changes in SaveCalendarTest #13

--- 2230/src/test/java/CalendarApp/core/CalendarSaveHandlerTest.java
+++ 2230/src/test/java/CalendarApp/core/CalendarSaveHandlerTest.java
@@ -1,40 +1,53 @@
 package CalendarApp.core;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 public class CalendarSaveHandlerTest {
 
 
-    public final static String SAVE_FOLDER = "/workspace/gr2230/2230/src/main/java/CalendarApp/core/savedCalendars/";
-
-    @Test
-    @DisplayName("Check that calendar is created and saved") 
-
-    public static void SaveCalendarTest() throws FileNotFoundException{
-        //Check if testCalendar exist, and delete it if it exists
-        String filePath = SAVE_FOLDER + "testCalendar.json";
+    private final static String SAVE_FOLDER = "/workspace/gr2230/2230/src/main/java/CalendarApp/core/savedCalendars/";
+    private static String testCalendarName = "testCalendar";
+    private static String filePath = SAVE_FOLDER + testCalendarName + ".json";
+    
+    //Check if testCalendar exsists, and delete it if so
+    @BeforeEach
+    public static void testSetup(){
         File file = new File(filePath);
         if(file.isFile()) {
             file.delete();
         }
-        assertFalse(file.exists());
-  
-        // Create and save a new calendar
-        CalendarSaveHandler.save("testCalendar");
-
-        // Check if new file was created
-        File newFile = new File(filePath);
-        assertTrue(newFile.exists());
-
+        assertFalse(file.isFile());
     }
 
+    @Test
+    @DisplayName("Check that correct file path is returned")
+    public static void getFilePathTest(){
+        assertEquals("/workspace/gr2230/2230/src/main/java/CalendarApp/core/savedCalendars/testCalendar.json", CalendarSaveHandler.getFilePath(testCalendarName));
+    }    
+
+    @Test
+    @DisplayName("Check that calendar is created and saved") 
+    public static void SaveCalendarTest(){
+        // Create and save a new calendar
+        try {
+            CalendarSaveHandler.save(testCalendarName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Check if new file was created
+        File file = new File(filePath);
+        assertTrue(file.exists());
+    }
 
 
 }
