bd6dad43262a2b55722dc60c89ce4dea4eb799ba
Endre Mork <endremor@stud.ntnu.no>
2022-10-04 16:13:36 +0200

    Created Calendar.Java
    Created Calendar.java.
    Added attributes, setters and getters
    
    Issue: #34

---
+++ 2230/core/src/main/java/calendarApp/core/Calendar.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+
+public class Calendar {
+
+    private String calendarName;
+    private ArrayList<Appointment> appointments = new ArrayList<Appointment>();
+
+
+    public Calendar(String calendarName) {
+        setCalendarName(calendarName);
+    }
+
+    // For loading of existing calendars
+    public Calendar(String calendarName, Appointment[] appointment) {
+        setCalendarName(calendarName);
+    }
+    
+    public String getCalendarName() {
+        return calendarName;
+    }
+
+    public void setCalendarName(String calendarName) {
+        this.calendarName = calendarName;
+    }
+
+    public ArrayList<Appointment> getAppointments() {
+        return appointments;
+    }
+
+
+
+}
