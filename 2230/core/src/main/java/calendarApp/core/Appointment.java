f82b212e2199bd69c8599f76161ba3e023e2d1e4
Endre Mork <endremor@stud.ntnu.no>
2022-10-04 16:34:14 +0200

    Create Appointment.java
    Create Appointment.java added attributes, setters and getters
    
    Issue: #36

---
+++ 2230/core/src/main/java/calendarApp/core/Appointment.java
@@ -0,0 +1,48 @@
+public class Appointment {
+
+    private String appointmentName;
+    private DaysOfTheWeek dayOfTheWeek;
+    private int startHour;
+    private int startMinute;
+    private int stopHour;
+    private int stopMinute;
+    
+    public Appointment(String appointmentName, DaysOfTheWeek dayOfTheWeek, int startHour , int stopHour, int startMinute, int stopMinute) {
+        this.appointmentName = appointmentName;
+        this.dayOfTheWeek = dayOfTheWeek;
+        setTime(startHour, stopHour, startMinute, stopMinute);
+    }
+    public void setAppointmentName(String appointmentName) {
+        this.appointmentName = appointmentName;
+    }
+    public void setDayOfTheWeek(DaysOfTheWeek dayOfTheWeek) {
+        this.dayOfTheWeek = dayOfTheWeek;
+    }
+
+    public void setTime(int startHour, int stopHour, int startMinute, int stopMinute){
+        this.startHour = startHour;
+        this.stopHour = stopHour;
+        this.startMinute = startMinute;
+        this.stopMinute = stopMinute;
+    }
+
+    public String getAppointmentName() {
+        return appointmentName;
+    }
+    public DaysOfTheWeek getDayOfTheWeek() {
+        return dayOfTheWeek;
+    }
+    public int getStartHour() {
+        return startHour;
+    }
+    public int getStartMinute() {
+        return startMinute;
+    }
+    public int getStopHour() {
+        return stopHour;
+    }
+    public int getStopMinute() {
+        return stopMinute;
+    }
+
+}
