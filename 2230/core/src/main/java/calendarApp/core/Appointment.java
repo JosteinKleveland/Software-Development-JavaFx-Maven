80d85c4cb51cfe98acf6b3b5d50eb3abdf8f3577
Endre Mork <endremor@stud.ntnu.no>
2022-10-04 21:29:10 +0200

    Added checks for inputs in Appointment.java
    Added checks for setter-methods and changed access modifier from public to private
    
    Issue: #36

--- 2230/core/src/main/java/calendarApp/core/Appointment.java
+++ 2230/core/src/main/java/calendarApp/core/Appointment.java
@@ -8,18 +8,29 @@ public class Appointment {
     private int stopMinute;
     
     public Appointment(String appointmentName, DaysOfTheWeek dayOfTheWeek, int startHour , int stopHour, int startMinute, int stopMinute) {
-        this.appointmentName = appointmentName;
-        this.dayOfTheWeek = dayOfTheWeek;
+        setAppointmentName(appointmentName);
+        setDayOfTheWeek(dayOfTheWeek);;
         setTime(startHour, stopHour, startMinute, stopMinute);
     }
-    public void setAppointmentName(String appointmentName) {
+
+    private void setAppointmentName(String appointmentName) {
+        if (!checkAppointmentName(appointmentName)){
+            throw new IllegalArgumentException("Given appintment name is blanc or have an illegal format. Only A-Y, a-y and 0-9 is allowed. The name must be at least 2 characters");
+        }
         this.appointmentName = appointmentName;
     }
-    public void setDayOfTheWeek(DaysOfTheWeek dayOfTheWeek) {
+    private void setDayOfTheWeek(DaysOfTheWeek dayOfTheWeek) {
+        if(!checkDayOfTheWeek(dayOfTheWeek)){
+            throw new IllegalArgumentException("Input for day is not valid");
+        }
+
         this.dayOfTheWeek = dayOfTheWeek;
     }
 
-    public void setTime(int startHour, int stopHour, int startMinute, int stopMinute){
+    private void setTime(int startHour, int stopHour, int startMinute, int stopMinute){
+        if(!checkTimeFormat(startHour, startMinute, stopHour, stopMinute)){
+            throw new IllegalArgumentException("Time does not have the correct format");
+        }
         this.startHour = startHour;
         this.stopHour = stopHour;
         this.startMinute = startMinute;
@@ -44,5 +55,42 @@ public class Appointment {
     public int getStopMinute() {
         return stopMinute;
     }
+
+    private boolean checkTimeFormat(int startHour,int stopHour,int startMinute,int stopMinute){
+        //
+        if (startHour < 0 || startHour > 23 || stopHour < 0 || stopHour > 23){
+            return false;
+        }
+
+        else if (startMinute > 59 || startMinute < 0 || stopMinute > 59 || stopMinute < 0){
+            return false;
+        }
+        // Check that an event is set to end after the start time
+        else if (startHour > stopHour){
+            return false;
+        }
+
+        else if (startHour == stopHour && startMinute >= stopMinute){
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean checkAppointmentName(String appointmentName){
+        if (appointmentName.isBlank() || appointmentName.matches("^[a-zA-Z0-9]*$") || appointmentName.length() < 2){
+            return false;
+        }
+            return true;
+    }
+
+    private boolean checkDayOfTheWeek(DayOfWeek day){
+        if (!day.getClass().isEnum()){
+            return false;
+        }
+
+        return true;
+    }
+
 
 }
