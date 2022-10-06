23a4e678f95c2e660fafafdb94a104e838001a4d
Endre Mork <endremor@stud.ntnu.no>
2022-10-06 18:15:17 +0200

    Added string values to DaysOfTheWeek
    Added string values to DaysOfTheWeek.Java
    
    Issue: #35

--- 2230/core/src/main/java/calendarApp/core/DaysOfTheWeek.java
+++ 2230/core/src/main/java/calendarApp/core/DaysOfTheWeek.java
@@ -2,12 +2,18 @@ package calendarApp.core;
 
 public enum DaysOfTheWeek {
     //Defining the 7 days of the week.
-    MONDAY,
-    TUESDAY, 
-    WEDENSDAY, 
-    THURSDAY, 
-    FRIDAY, 
-    SATURDAY, 
-    SUNDAY;
+    MONDAY("Monday"),
+    TUESDAY("Tuesday"), 
+    WEDENSDAY("Wedensday"), 
+    THURSDAY("Thursday"), 
+    FRIDAY("Friday"), 
+    SATURDAY("Saturday"), 
+    SUNDAY("Sunday");
 
+
+    final String nameOfDay;
+
+    DaysOfTheWeek (String nameOfDay){
+        this.nameOfDay = nameOfDay;
+    }
 }
