827eab5208a86ff4601566ed2ac657b0b8222042
Simen <simeeid@stud.ntnu.no>
2022-10-25 10:15:44 +0200

    Renamed and added some methods
    Renamed logicService and adde some of the methods to be implemented
    
    Issue: #65

---
+++ 2230/restapi/src/main/java/calendarApp/restapi/CalendarLogicService.java
@@ -0,0 +1,26 @@
+package calendarApp.restapi;
+
+import calendarApp.core.CalendarLogic;
+
+/**
+ * The top-level rest service for CalendarLogic
+ */
+@Path(CalendarLogicService.CALENDAR_LOGIC_SERVICE_PATH)
+public class CalendarLogicService {
+    //prefix for all requests that are sent to instances of this particular class
+    public static final String CALENDAR_LOGIC_SERVICE_PATH = "calendar";
+
+    //Logger?
+
+    //@Inject?
+    @Context
+    private CalendarLogic calendarLogic;
+
+    @GET
+    public CalendarLogic getCalendarLogci() {
+        //LOG.debug ...
+        return calendarLogic;
+    }
+
+
+}
