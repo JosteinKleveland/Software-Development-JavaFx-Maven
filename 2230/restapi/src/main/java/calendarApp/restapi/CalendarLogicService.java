a80d191dce7d15ea45f65a35312839d1f61c0d71
Simen Eidal <simeeid@stud.ntnu.no>
2022-10-26 13:16:13 +0000

    Updated CalendarLogicService
    Added new imports and methods to logicService
    
    Issue: #65

--- 2230/restapi/src/main/java/calendarApp/restapi/CalendarLogicService.java
+++ 2230/restapi/src/main/java/calendarApp/restapi/CalendarLogicService.java
@@ -1,11 +1,20 @@
 package calendarApp.restapi;
 
 import calendarApp.core.CalendarLogic;
+import calendarApp.json.CalendarSaveHandler;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
 
 /**
  * The top-level rest service for CalendarLogic
  */
 @Path(CalendarLogicService.CALENDAR_LOGIC_SERVICE_PATH)
+@Produces(MediaType.APPLICATION_JSON)
 public class CalendarLogicService {
     //prefix for all requests that are sent to instances of this particular class
     public static final String CALENDAR_LOGIC_SERVICE_PATH = "calendar";
@@ -16,10 +25,47 @@ public class CalendarLogicService {
     @Context
     private CalendarLogic calendarLogic;
 
+    @Context
+    private CalendarSaveHandler calendarSaveHandler;
+
+    /**
+     * The root resource, i.e /calendar
+     * 
+     * @return the CalendarLogic
+     */
     @GET
-    //@Produces(MediaType.APPLICATION_JSON)
     public CalendarLogic getCalendarLogic() {
         //LOG.debug ...
         return calendarLogic;
     }
+
+    /**
+     * The root resource, i.e. /calendar
+     * 
+     * @return the CalendarLogic
+     */
+    /*@Path("/settings")
+    public TodoSettingsResource getTodoSettings() {
+        LOG.debug("Sub-resource for TodoSettings");
+        return new TodoSettingsResource(todoModel);
+    }*/ 
+    //Tror denne ikke er nødvndig for vår del? Henter ut settings elns.
+
+    /**
+   * Returns the CalendarResource containing the
+   * currentCalendar in the CalendarLogic
+   * (as a resource to support chaining path elements).
+   * This supports all requests referring to TodoLists by name.
+   * Note that the Calendar needn't exist, since it can be a PUT.
+   *
+   * @param name the name of the calendar
+   */
+  @Path("/list/{name}")
+  public CalendarResource getCalendar(@PathParam("name") String name) {
+    Calendar calendar = getCalendarLogic().getCurrentCalendar();
+    CalendarResource calendarResource = new CalendarResource(calendarLogic, name, calendar);
+    calendarResource.setCalendarSaveHandler(calendarSaveHandler);
+    return calendarResource;
+  }
 }
+
