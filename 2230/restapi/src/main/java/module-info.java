d4c492bffaff4f1256bbd86f8b8ca16f7c6a070f
Simen Eidal <simeeid@stud.ntnu.no>
2022-10-26 13:14:41 +0000

    Initial implementation of module-info
    Initial implementation of the module-info in restapi
    
    Issue: #68

--- 2230/restapi/src/main/java/module-info.java
+++ 2230/restapi/src/main/java/module-info.java
@@ -1,3 +1,15 @@
-module name {
-    
+module calendarApp.restapi {
+    //requires transitive com.fasterxml.jackson.databind;
+
+    exports calendarApp.restapi;
+
+    requires jakarta.ws.rs;
+
+    requires jersey.common;
+    requires jersey.server;
+    requires jersey.media.json.jackson;
+
+    requires org.glassfish.hk2.api;
+    requires org.slf4j;
+
 }
