a930b08369027708a71e8efb9a23b034f3d3abff
William Andreas Krohn Marleau <wamarlea@stud.ntnu.no>
2022-11-02 14:32:34 +0000

    Set up framework for WelcomeWindowController
    Added fxml connections and empty methods. Also changed fxml onAction
    from saveCalendar to newCalendar.
    
    Issues: 87

--- 2230/fxui/src/main/java/calendarApp/ui/WelcomeWindowController.java
+++ 2230/fxui/src/main/java/calendarApp/ui/WelcomeWindowController.java
@@ -1,5 +1,27 @@
 package calendarApp.ui;
 
+import calendarApp.core.Calendar;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class WelcomeWindowController {
     
+    @FXML private Button btnNewCalendar;
+    @FXML private Button btnLoadCalendar;
+
+    @FXML private TextField txtCalendarNameInput; 
+
+    public Calendar newCalendar() {
+        
+        
+        return null;     
+    }
+
+    public Calendar loadCalendar() {
+
+        return null;
+    }
+
 }
