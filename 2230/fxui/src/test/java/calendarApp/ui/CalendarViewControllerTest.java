0cf364c9d665e3cc7a1fdae2daefc682be6d1e09
William Andreas Krohn Marleau <wamarlea@stud.ntnu.no>
2022-11-12 19:40:05 +0000

    Added setup for CalendarViewControllerTest
    Added Override start and BeforeEach setup
    
    Issues: #126

---
+++ 2230/fxui/src/test/java/calendarApp/ui/CalendarViewControllerTest.java
@@ -0,0 +1,51 @@
+package calendarApp.ui;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.internal.matchers.Each;
+import org.junit.jupiter.api.BeforeEach;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import calendarApp.core.Appointment;
+import calendarApp.core.Calendar;
+import calendarApp.core.DaysOfTheWeek;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CalendarViewControllerTest extends ApplicationTest {
+
+    private CalendarViewController controller;
+    private Calendar calendar;
+    
+    @Override
+    public void start(final Stage stage) throws Exception {
+      final FXMLLoader loader = new FXMLLoader(getClass().getResource("CalendarViewController.fxml"));
+      final Parent root = loader.load();
+      this.controller = loader.getController();
+      stage.setScene(new Scene(root));
+      stage.show();
+    }
+
+    @BeforeEach
+    public void setupAppointments() throws IOException {
+        this.calendar = new Calendar("New Calendar");
+
+        Appointment appointment1 = new Appointment("Fotball", "Husk å ta med fotball", DaysOfTheWeek.MONDAY, 18, 19, 0, 30);
+        Appointment appointment2 = new Appointment("Yoga", "Ved blåbærvegen 8", DaysOfTheWeek.WEDNESDAY, 17, 18, 0, 30);
+        Appointment appointment3 = new Appointment("Trumpet", "Husk munnstykke", DaysOfTheWeek.MONDAY, 13, 14, 0, 0);
+        
+        calendar.addAppointment(appointment1);
+        calendar.addAppointment(appointment2);
+        calendar.addAppointment(appointment3);
+
+        controller.initialize(calendar);
+    }
+}
